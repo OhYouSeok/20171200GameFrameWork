3ad82ae513378f3e72865b17240d4bf8e46d2d31
id40792604 <id40792604@gmail.com>
2018-11-13 12:50:35 +0900

    PP05.AnimatingSprite

--- PP01.HelloSDL/Main.cpp
+++ PP01.HelloSDL/Main.cpp
@@ -1,11 +1,10 @@
-#pragma once
 #include "Game.h"
 #include "SDL.h"
 
 Game * g_game = 0;
 int main(int argc, char* argv[]) {
 	g_game = new Game();
-	g_game->init("PP04", 100, 100, 1024, 720, false);
+	g_game->init("PP05.AnimatingSprite", 100, 100, 1024, 720, false);
 	while (g_game->running()) {
 		g_game->handleEvents();
 		g_game->update();
