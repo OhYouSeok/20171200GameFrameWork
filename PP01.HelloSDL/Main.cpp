968b0f0003e496e7a82bdf19c3d992931654ff29
id40792604 <id40792604@gmail.com>
2018-11-13 12:45:32 +0900

    PP03.GameClass

--- PP01.HelloSDL/Main.cpp
+++ PP01.HelloSDL/Main.cpp
@@ -1,40 +1,17 @@
-#include<SDL.h>
+#pragma once
+#include "Game.h"
+#include "SDL.h"
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-
-bool init(const char *title, int xpos, int ypos, int width, int height, int flags) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-		else { return false; }
-	}
-	return true;
-}
-void render(){
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 127, 255);
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
-}
-void update() {}
-void handleEvents(){}
-void clean(){}
-bool g_bRunning = false;
-
+Game * g_game = 0;
 int main(int argc, char* argv[]) {
-	if (init("Game FrameWork", SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		1024, 768, SDL_WINDOW_SHOWN)) {
-		g_bRunning = true;
+	g_game = new Game();
+	g_game->init("PP04", 100, 100, 1024, 720, false);
+	while (g_game->running()) {
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
 	}
-	else { return 1; }
-	while (g_bRunning) {
-		render();
-	}
-	SDL_Quit();
+
+	g_game->clean();
 	return 0;
 }
