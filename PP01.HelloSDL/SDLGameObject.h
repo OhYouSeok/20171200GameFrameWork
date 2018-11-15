936cbaff2e5d1b38af4b368df72b86970d37532b
유석 오 <20171200@vision.hoseo.edu>
2018-11-15 12:08:36 +0900

    PP12. Vector2D

--- PP01.HelloSDL/SDLGameObject.h
+++ PP01.HelloSDL/SDLGameObject.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "GameObject.h"
+#include "Vector2D.h"
 
 class SDLGameObject : public GameObject
 {
@@ -8,13 +9,15 @@ public:
 	virtual void draw();
 	virtual void update() = 0;
 	virtual void clean() = 0;
-
+	SDL_Rect getRect();
+	std::string getTextureID() const;
 protected:
-	int m_x;
-	int m_y;
 	int m_width;
 	int m_height;
 	int m_currentRow;
 	int m_currentFrame;
 	std::string m_textureID;
+	Vector2D m_position;
+	Vector2D m_velocity;
+	Vector2D m_acceleration;
 };
